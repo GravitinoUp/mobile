497b50afcf81602b0100dca8c8d1042d16b73263
ION <thegoshagames@gmail.com>
2024-03-26 08:52:50 +0300

    eslint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
         node: true,
     },
     extends: [
-        'standard-with-typescript',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
